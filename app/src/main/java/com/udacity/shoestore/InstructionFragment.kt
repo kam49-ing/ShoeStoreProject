eb54338b549198df8813d2e5795ba226667bbc21
Ngomdé Cadet KAMDAOU <nkamdaou@gmail.ocm>
2021-09-29 20:58:29 +0200

    I add instruction fragment with all dependencies

--- app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
@@ -1,20 +1,20 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
 class InstructionFragment : Fragment() {
     //private lateinit var binding: FragmentInstructionBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding: FragmentInstructionBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_instruction, container, false)
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+        val binding: FragmentInstructionBinding = FragmentInstructionBinding.inflate(inflater, container, false)
+        //val binding: FragmentInstructionBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_instruction, container, false)
+        return binding.root
     }
 
 }
